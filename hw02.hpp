8467130528b6a3344f112d2023a3f06ded4ca17d
Ken Dai <metalhead.ken77@gmail.com>
2019-04-03 14:00:41 +0000

    Implement the plane Identification
    - Conduct 3D standard Hough Transform

--- hw02.hpp
+++ hw02.hpp
@@ -1,20 +1,58 @@
 #pragma once
 #include <cstring>
+#include <cmath>
+#include <vector>
+
+struct point {
+    double x;
+    double y;
+    double z;
+};
+
+struct color {
+    int R;
+    int G;
+    int B;
+};
 
 struct mydata {
+    size_t size;
+    struct point *point;
 };
+
 bool readPointCloud(const char *filename, mydata& data);
 double centerPointCloudToOrigin(mydata &data);
 
 struct accumulator {
+    size_t ***accum;
+
+    size_t n_theta;
+    size_t n_phi;
+    size_t n_rho;
+
+    double rho_max;
+
+    double d_theta;
+    double d_phi;
+    double d_rho;
 };
+
 void prepareAccumulator(accumulator& votes, const double rho_max, const size_t n_theta, const size_t n_phi, const size_t n_rho);
 void releaseAccumulator(accumulator& votes);
 
 void houghTransform(const mydata &data, accumulator &votes);
 
 // This struct should store resultant plane parameters in descending order
+struct houghPlane {
+    size_t votes;
+
+    double theta;
+    double phi;
+    double rho;
+};
+
 struct houghPlanes {
+    std::vector<houghPlane> planes;
 };
 
 void identifyPlaneParameters(const accumulator &votes, const size_t threshold, houghPlanes &planes);
