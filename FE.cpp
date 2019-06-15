89dbb79ae3a4d2e94d864e4274423a65e658f9d1
Ken Dai <metalhead.ken77@gmail.com>
2019-06-15 17:40:14 +0000

    Implement the Circle Hough Transform step 1, 2, 3, 4
    - Implement read PNG image file
    - Implement convert RGB to gray-scale image
    - Implement the Sobel filter
    - Implement prepare accumulator

--- FE.cpp
+++ FE.cpp
@@ -11,34 +11,72 @@ int readImage(const char *filename, inputImage &png) {
 }
 
 void toGrayScale(const inputImage& input, outputImage &output) {
-    cout << "\nModify toGrayScale function in " << __FILE__ << " @ " << __LINE__;
-    // The following code only use the blue intensity as the output
     output.width = input.width; 
     output.height = input.height; 
     output.pixels = new png_byte[output.width * output.height]; 
     for(int y=0;y<input.height;++y) {
         for(int x=0;x<input.width;++x) {
-            output.pixels[y*input.width+x] = input.row_pointers[y][4*x+2]; 
+            unsigned char r = input.row_pointers[y][4 * x];
+            unsigned char g = input.row_pointers[y][4 * x + 1];
+            unsigned char b = input.row_pointers[y][4 * x + 2];
+
+            // Turn RGBA into gray-scale image
+            output.pixels[y*input.width+x] = 0.299 * r + 0.587 * g + 0.114 * b; 
         }
     }
 }
 
 // https://en.wikipedia.org/wiki/Sobel_operator
 void toEdge(const outputImage &input, outputImage &output) {
-    cout << "\nModify toEdge function in " << __FILE__ << " @ " << __LINE__; 
-    // The following code only copies input image to the output image
+    const short GX[3][3] = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+    const short GY[3][3] = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+    
     output.width = input.width; 
     output.height = input.height; 
     output.pixels = new png_byte[output.width * output.height]; 
-    for(png_uint_32 y=0;y<input.height;++y) {
-        for(png_uint_32 x=0;x<input.width;++x) {
-            output.pixels[y*input.width+x] = input.pixels[y*input.width+x]; 
+
+    for(png_uint_32 y = 1; y < input.height - 1; ++y) {
+        for(png_uint_32 x = 1; x < input.width - 1; ++x) {
+            short mask[3][3], gx = 0, gy = 0;
+
+            mask[0][0] = input.pixels[(y - 1) * input.width + (x - 1)];
+            mask[0][1] = input.pixels[(y - 1) * input.width + (x    )];
+            mask[0][2] = input.pixels[(y - 1) * input.width + (x + 1)];
+            mask[1][0] = input.pixels[(y    ) * input.width + (x - 1)];
+            mask[1][1] = input.pixels[(y    ) * input.width + (x    )];
+            mask[1][2] = input.pixels[(y    ) * input.width + (x + 1)];
+            mask[2][0] = input.pixels[(y + 1) * input.width + (x - 1)];
+            mask[2][1] = input.pixels[(y + 1) * input.width + (x    )];
+            mask[2][2] = input.pixels[(y + 1) * input.width + (x + 1)];
+
+            for (png_uint_16 i = 0; i < 3; i++) {
+                for (png_uint_16 j = 0; j < 3; j++) {
+                    gx += mask[i][j] * GX[i][j];
+                    gy += mask[i][j] * GY[i][j];
+                }
+            }
+
+            double g = sqrt(gx * gx + gy * gy);
+            g = (g < 0) ? 0 : (g > 255) ? 255 : g;
+            
+            output.pixels[y * input.width + x] = (unsigned char) g; 
         }
-    }    
+    }
 }
 
 void prepareAccumulator (const outputImage &img, uint32_t r_min, uint32_t r_max, uint32_t r_step, uint8_t pixel_threshold, double threshold, accumulator& votes) {
-    cout << "\nImplement prepareAccumulator function in " << __FILE__ << " @ " << __LINE__; 
+    votes.r_min = r_min;
+    votes.r_max = r_max;
+    votes.r_step = r_step;
+
+    votes.pixel_threshold = pixel_threshold;
+    votes.threshold = threshold;
+
+    votes.width = img.width;
+    votes.height = img.height;
+    votes.nRadii = (r_max - r_min) / r_step + 1;
+
+    votes.accum = new size_t [votes.width * votes.height * votes.nRadii]();
 }
 
 uint32_t CHT(const outputImage &input, accumulator& votes) {
@@ -63,5 +101,5 @@ uint32_t extractCircles(accumulator& votes, circles& circles) {
 }
 
 void freeAccumulator(accumulator& votes) {
-    cout << "\nImplement freeAccumulator function in " << __FILE__ << " @ " << __LINE__;
+    delete [] votes.accum;
 }
