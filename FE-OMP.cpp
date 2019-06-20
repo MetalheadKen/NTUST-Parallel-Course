0cb80bac970ad98e06922ab93a63cce656bfc740
Ken Dai <metalhead.ken77@gmail.com>
2019-06-20 09:31:24 +0000

    Implement the Circle Hough Transform OpenMP version
    - Using OpenMP library
    - Improve the all performance by 288.4%
      - Improve the t0 performance by 120.8%
      - Improve the t1 performance by 320.6%
      - Improve the t2 performance by 483.3%
      - Improve the t3 performance by 264.0%
    - Remove cout hints from the serial version

--- FE-OMP.cpp
+++ FE-OMP.cpp
@@ -1,67 +1,228 @@
-#include "FE.hpp"
+#include "FE-OMP.hpp"
 #include "PNGio.hpp"
 #include <cmath>
 #include <iostream>
+#include <algorithm>
 using std::cout; 
 // 
 int readImage(const char *filename, inputImage &png) {
-    cout << "\n[OMP]Check readImage in " << __FILE__ << " @ " << __LINE__; 
     // If you are okay with how image data is stored (defined in PNGio.cpp), you can simply use the following line of code for this function
     return pngRead(filename, png); 
 }
 
 void toGrayScale(const inputImage& input, outputImage &output) {
-    cout << "\n[OMP]Modify toGrayScale function in " << __FILE__ << " @ " << __LINE__;
-    // The following code only use the blue intensity as the output
     output.width = input.width; 
     output.height = input.height; 
     output.pixels = new png_byte[output.width * output.height]; 
-    for(int y=0;y<input.height;++y) {
-        for(int x=0;x<input.width;++x) {
-            output.pixels[y*input.width+x] = input.row_pointers[y][4*x+2]; 
+
+    #pragma omp parallel for default(none) firstprivate(input) shared(output) collapse(2)
+    for(int y = 0; y < input.height; ++y) {
+        for(int x = 0; x < input.width; ++x) {
+            unsigned char r = input.row_pointers[y][4 * x];
+            unsigned char g = input.row_pointers[y][4 * x + 1];
+            unsigned char b = input.row_pointers[y][4 * x + 2];
+
+            // Turn RGBA into gray-scale image
+            output.pixels[y*input.width+x] = 0.299 * r + 0.587 * g + 0.114 * b; 
         }
     }
 }
 
 // https://en.wikipedia.org/wiki/Sobel_operator
 void toEdge(const outputImage &input, outputImage &output) {
-    cout << "\n[OMP]Modify toEdge function in " << __FILE__ << " @ " << __LINE__; 
-    // The following code only copies input image to the output image
+    const short GX[3][3] = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+    const short GY[3][3] = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+    
     output.width = input.width; 
     output.height = input.height; 
-    output.pixels = new png_byte[output.width * output.height]; 
-    for(png_uint_32 y=0;y<input.height;++y) {
-        for(png_uint_32 x=0;x<input.width;++x) {
-            output.pixels[y*input.width+x] = input.pixels[y*input.width+x]; 
+    output.pixels = new png_byte [output.width * output.height]; 
+    
+    #pragma omp parallel for default(none) firstprivate(input, GX, GY) shared(output) collapse(2)
+    for(png_uint_32 y = 1; y < input.height - 1; ++y) {
+        for(png_uint_32 x = 1; x < input.width - 1; ++x) {
+            short mask[3][3], gx = 0, gy = 0;
+
+            mask[0][0] = input.pixels[(y - 1) * input.width + (x - 1)];
+            mask[0][1] = input.pixels[(y - 1) * input.width + (x    )];
+            mask[0][2] = input.pixels[(y - 1) * input.width + (x + 1)];
+            mask[1][0] = input.pixels[(y    ) * input.width + (x - 1)];
+            mask[1][1] = input.pixels[(y    ) * input.width + (x    )];
+            mask[1][2] = input.pixels[(y    ) * input.width + (x + 1)];
+            mask[2][0] = input.pixels[(y + 1) * input.width + (x - 1)];
+            mask[2][1] = input.pixels[(y + 1) * input.width + (x    )];
+            mask[2][2] = input.pixels[(y + 1) * input.width + (x + 1)];
+
+            for (png_uint_16 i = 0; i < 3; i++) {
+                for (png_uint_16 j = 0; j < 3; j++) {
+                    gx += mask[i][j] * GX[i][j];
+                    gy += mask[i][j] * GY[i][j];
+                }
+            }
+
+            double g = sqrt(gx * gx + gy * gy);
+            g = (g < 0) ? 0 : (g > 255) ? 255 : g;
+            
+            output.pixels[y * input.width + x] = (unsigned char) g; 
         }
-    }    
+    }
 }
 
 void prepareAccumulator (const outputImage &img, uint32_t r_min, uint32_t r_max, uint32_t r_step, uint8_t pixel_threshold, double threshold, accumulator& votes) {
-    cout << "\n[OMP]Implement prepareAccumulator function in " << __FILE__ << " @ " << __LINE__; 
+    votes.r_min = r_min;
+    votes.r_max = r_max;
+    votes.r_step = r_step;
+
+    votes.pixel_threshold = pixel_threshold;
+    votes.threshold = threshold;
+
+    votes.width = img.width;
+    votes.height = img.height;
+    votes.nRadii = (r_max - r_min) / r_step + 1;
+
+    votes.accum = new size_t [votes.width * votes.height * votes.nRadii]();
+}
+
+static inline void _vote(accumulator& votes, int x, int y, int r, uint32_t& total) {
+    if ((x > 0 && (uint32_t) x < votes.width) && (y > 0 && (uint32_t) y < votes.height)) {
+        votes.accum[y * votes.width + x + r * votes.width * votes.height]++;
+        total++;
+    }
 }
 
 uint32_t CHT(const outputImage &input, accumulator& votes) {
-    cout << "\n[OMP]Implement CHT function in " << __FILE__ << " @ " << __LINE__; 
+    uint32_t total_votes = 0;
+    
     // for each pixel that are above pixel_threshold, cast votes 
+    #pragma omp parallel reduction(+:total_votes) default(none) firstprivate(input) shared(votes)
+    {
+        #pragma omp for collapse(2)
+        for (png_uint_32 y = 0; y < input.height; y++) {
+            for (png_uint_32 x = 0; x < input.width; x++) {
+                uint32_t pos = y * input.width + x;
+
+                if (input.pixels[pos] > votes.pixel_threshold) {
+                    for (uint32_t r = 0; r < votes.nRadii; r++) {
+                        uint32_t radii = votes.r_min + r * votes.r_step;
+
+                        _vote(votes, x, y - radii, r, total_votes);
+                        _vote(votes, x, y + radii, r, total_votes);
+                        _vote(votes, x - radii, y, r, total_votes);
+                        _vote(votes, x + radii, y, r, total_votes);
+
+                        for (uint32_t dy = 1; dy < 0.71 * radii; dy++) {
+                            const uint32_t dx = radii * sqrt(1.0 - (double) dy * dy / (radii * radii));
+
+                            _vote(votes, x - dx, y - dy, r, total_votes);
+                            _vote(votes, x - dx, y + dy, r, total_votes);
+                            _vote(votes, x + dx, y - dy, r, total_votes);
+                            _vote(votes, x + dx, y + dy, r, total_votes);
+
+                            _vote(votes, x - dy, y - dx, r, total_votes);
+                            _vote(votes, x - dy, y + dx, r, total_votes);
+                            _vote(votes, x + dy, y - dx, r, total_votes);
+                            _vote(votes, x + dy, y + dx, r, total_votes);
+
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     // After voting, filter significant votes based on the criterion specified in the instruction of the exam
     // each significant vote becomes an identified circle
+    const double vote_threshold = votes.threshold * total_votes / (votes.width * votes.height * votes.nRadii);
+    #pragma omp parallel default(none) firstprivate(input, vote_threshold) shared(votes)
+    {
+        #pragma omp for collapse(3)
+        for (png_uint_32 y = 0; y < input.height; y++) {
+            for (png_uint_32 x = 0; x < input.width; x++) {
+                for (uint32_t r = 0; r < votes.nRadii; r++) {
+                    uint32_t pos = y * input.width + x + r * input.width * input.height;
+
+                    if (votes.accum[pos] > vote_threshold) {
+                        struct circle circle;
+
+                        circle.x     = x;
+                        circle.y     = y;
+                        circle.r     = votes.r_min + r * votes.r_step;
+                        circle.votes = votes.accum[pos];
+
+                        #pragma omp critical (dataupdate)
+                        {
+                            votes.circles.push_back(circle);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    sort(votes.circles.begin(), votes.circles.end(),
+        [](const circle& a, const circle& b) {
+            if (a.x == b.x)
+                return a.y > b.y;
+            else
+                return a.x > b.x;
+        }
+    );
     
     // return the number of identified circles at this stage.
-    return 0; 
+    return votes.circles.size(); 
 }
 
 uint32_t extractCircles(accumulator& votes, circles& circles) {
-    cout << "\n[OMP]Implement extractCircles function in " << __FILE__ << " @ " << __LINE__; 
+    const int diff_dist = 20;
+    bool *is_merge = new bool [votes.circles.size()]();
+
+    // Find the similar circle and only pick one of them
+    for (uint32_t i = 0; i < votes.circles.size(); i++) {
+        if (is_merge[i]) continue;
+
+        is_merge[i] = true;
+        auto c1 = votes.circles[i];
+
+        std::vector<circle> similar;
+        similar.push_back(c1);
+
+        for (uint32_t j = 0; j < votes.circles.size(); j++) {
+            if (is_merge[j]) continue;
+
+            auto c2 = votes.circles[j];
+
+            if (abs(c1.x - c2.x) < diff_dist && 
+                abs(c1.y - c2.y) < diff_dist &&
+                abs(c1.r - c2.r) < diff_dist) {
+                is_merge[j] = true;
+                similar.push_back(c2);
+            }
+        }
+
+        // Average all circles parameters
+        struct circle sum = { 0 };
+        for (const auto& c : similar) {
+            sum.x     += c.x;
+            sum.y     += c.y;
+            sum.r     += c.r;
+            sum.votes += c.votes;
+        }
+
+        int size = similar.size();
+        sum.x     /= size;
+        sum.y     /= size;
+        sum.r     /= size;
+        sum.votes /= size;
+
+        circles.data.push_back({sum.x, sum.y, sum.r, sum.votes});
+    }
     
     // the following line of code demonstrate how to put detected circles into the circles object
     // you shoudl remove it after you have extract circles from votes...
-    circles.data.push_back({100, 150, 200, 1000}); // store a circle @ (100, 150) with radius 200 and 1000 votes.
+    // circles.data.push_back({100, 150, 200, 1000}); // store a circle @ (100, 150) with radius 200 and 1000 votes.
 
-    return 0; 
+    return circles.data.size(); 
 }
 
 void freeAccumulator(accumulator& votes) {
-    cout << "\n[OMP]Implement freeAccumulator function in " << __FILE__ << " @ " << __LINE__;
+    delete [] votes.accum;
 }
