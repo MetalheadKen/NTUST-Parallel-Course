225d45757bb2badfd010cf0d55727376a8c47f47
Ken Dai <metalhead.ken77@gmail.com>
2019-06-20 21:50:55 +0000

    Implement the Circle Hough Transform OpenCL version
    - Implement the OpenCL version
    - Using OpenCL library

--- FE-OCL.cl
+++ FE-OCL.cl
@@ -1,1 +1,52 @@
+#pragma OPENCL EXTENSION cl_khr_int64_base_atomics:enable
+#pragma OPENCL EXTENSION cl_khr_fp:enable
+
 // Put your OpenCL kernel code in this file.
+void _vote(
+    __global uint *accum, 
+    uint x, uint y, uint r, uint width, uint height, __global uint *cnt) {
+    if ((x > 0 && x < width) && (y > 0 && y < height)) {
+        atomic_inc(accum + (y * width + x + r * width * height));
+        atomic_inc(cnt);
+    }
+}
+
+void CHT_vote(
+    uint width, uint height, uint nRadii,
+    uint r_min, uint r_step, uint pixel_threshold,
+    __global uint *pixels, __global uint *accum, __global uint *cnt) {
+    uint x = get_global_id(0);
+    uint y = get_global_id(1);
+
+    // for each pixel that are above pixel_threshold, cast votes 
+    if ((x > 0 && x < width) && (y > 0 && y < height)) {
+        uint pos = y * input.width + x;
+
+        if (pixels[pos] > pixel_threshold) {
+            for (uint r = 0; r < votes.nRadii; r++) {
+                uint radii = r_min + r * r_step;
+
+                _vote(accum, x, y - radii, r, width, height, cnt);
+                _vote(accum, x, y + radii, r, width, height, cnt);
+                _vote(accum, x - radii, y, r, width, height, cnt);
+                _vote(accum, x + radii, y, r, width, height, cnt);
+
+                for (uint32_t dy = 1; dy < 0.71 * radii; dy += 8) {
+                    const uint32_t dx = radii * sqrt(1.0 - (double) dy * dy / (radii * radii));
+
+                    _vote(accum, x - dx, y - dy, r, width, height, cnt);
+                    _vote(accum, x - dx, y + dy, r, width, height, cnt);
+                    _vote(accum, x + dx, y - dy, r, width, height, cnt);
+                    _vote(accum, x + dx, y + dy, r, widht, height, cnt);
+
+                    _vote(accum, x - dy, y - dx, r, width, height, cnt);
+                    _vote(accum, x - dy, y + dx, r, width, height, cnt);
+                    _vote(accum, x + dy, y - dx, r, width, height, cnt);
+                    _vote(accum, x + dy, y + dx, r, width, height, cnt);
+
+                }
+            }
+        }
+    }
+}
+
--- FE-OCL.cpp
+++ FE-OCL.cpp
@@ -1,67 +1,216 @@
-#include "FE.hpp"
+#include "FE-OCL.hpp"
 #include "PNGio.hpp"
+#include "YoUtil.hpp"
 #include <cmath>
 #include <iostream>
 using std::cout; 
 // 
 int readImage(const char *filename, inputImage &png) {
-    cout << "\n[OCL]Check readImage in " << __FILE__ << " @ " << __LINE__; 
     // If you are okay with how image data is stored (defined in PNGio.cpp), you can simply use the following line of code for this function
     return pngRead(filename, png); 
 }
 
 void toGrayScale(const inputImage& input, outputImage &output) {
-    cout << "\n[OCL]Modify toGrayScale function in " << __FILE__ << " @ " << __LINE__;
-    // The following code only use the blue intensity as the output
     output.width = input.width; 
     output.height = input.height; 
     output.pixels = new png_byte[output.width * output.height]; 
     for(int y=0;y<input.height;++y) {
         for(int x=0;x<input.width;++x) {
-            output.pixels[y*input.width+x] = input.row_pointers[y][4*x+2]; 
+            unsigned char r = input.row_pointers[y][4 * x];
+            unsigned char g = input.row_pointers[y][4 * x + 1];
+            unsigned char b = input.row_pointers[y][4 * x + 2];
+
+            // Turn RGBA into gray-scale image
+            output.pixels[y*input.width+x] = 0.299 * r + 0.587 * g + 0.114 * b; 
         }
     }
 }
 
 // https://en.wikipedia.org/wiki/Sobel_operator
 void toEdge(const outputImage &input, outputImage &output) {
-    cout << "\n[OCL]Modify toEdge function in " << __FILE__ << " @ " << __LINE__; 
-    // The following code only copies input image to the output image
+    const short GX[3][3] = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+    const short GY[3][3] = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+    
     output.width = input.width; 
     output.height = input.height; 
-    output.pixels = new png_byte[output.width * output.height]; 
-    for(png_uint_32 y=0;y<input.height;++y) {
-        for(png_uint_32 x=0;x<input.width;++x) {
-            output.pixels[y*input.width+x] = input.pixels[y*input.width+x]; 
+    output.pixels = new png_byte [output.width * output.height]; 
+
+    for(png_uint_32 y = 1; y < input.height - 1; ++y) {
+        for(png_uint_32 x = 1; x < input.width - 1; ++x) {
+            short mask[3][3], gx = 0, gy = 0;
+
+            mask[0][0] = input.pixels[(y - 1) * input.width + (x - 1)];
+            mask[0][1] = input.pixels[(y - 1) * input.width + (x    )];
+            mask[0][2] = input.pixels[(y - 1) * input.width + (x + 1)];
+            mask[1][0] = input.pixels[(y    ) * input.width + (x - 1)];
+            mask[1][1] = input.pixels[(y    ) * input.width + (x    )];
+            mask[1][2] = input.pixels[(y    ) * input.width + (x + 1)];
+            mask[2][0] = input.pixels[(y + 1) * input.width + (x - 1)];
+            mask[2][1] = input.pixels[(y + 1) * input.width + (x    )];
+            mask[2][2] = input.pixels[(y + 1) * input.width + (x + 1)];
+
+            for (png_uint_16 i = 0; i < 3; i++) {
+                for (png_uint_16 j = 0; j < 3; j++) {
+                    gx += mask[i][j] * GX[i][j];
+                    gy += mask[i][j] * GY[i][j];
+                }
+            }
+
+            double g = sqrt(gx * gx + gy * gy);
+            g = (g < 0) ? 0 : (g > 255) ? 255 : g;
+            
+            output.pixels[y * input.width + x] = (unsigned char) g; 
         }
-    }    
+    }
 }
 
 void prepareAccumulator (const outputImage &img, uint32_t r_min, uint32_t r_max, uint32_t r_step, uint8_t pixel_threshold, double threshold, accumulator& votes) {
-    cout << "\n[OCL]Implement prepareAccumulator function in " << __FILE__ << " @ " << __LINE__; 
+    votes.r_min = r_min;
+    votes.r_max = r_max;
+    votes.r_step = r_step;
+
+    votes.pixel_threshold = pixel_threshold;
+    votes.threshold = threshold;
+
+    votes.width = img.width;
+    votes.height = img.height;
+    votes.nRadii = (r_max - r_min) / r_step + 1;
+
+    votes.accum = new size_t [votes.width * votes.height * votes.nRadii]();
 }
 
 uint32_t CHT(const outputImage &input, accumulator& votes) {
-    cout << "\n[OCL]Implement CHT function in " << __FILE__ << " @ " << __LINE__; 
-    // for each pixel that are above pixel_threshold, cast votes 
+    uint32_t total_votes = 0;
+
+    cl::CommandQueue cmdQueue;
+    cl::Context context;
+    cl::Program *program;
+
+    // Prepare GPU
+    try {
+        YoUtil::GPU::verbose = false;
+        YoUtil::GPU gpu(CL_DEVICE_TYPE_GPU, false, "");
+        gpu.addProgramByFile("FE-OCL", "FE-OCL.cl");
+        cmdQueue = gpu.getCommandQueue(0);
+        context = cmdQueue.getInfo<CL_QUEUE_CONTEXT>();
+        program = gpu.getProgram("FE-OCL"); 
+    } catch (cl::Error &e) {
+        std::cerr << "CHT: " << e.what() << std::endl;
+        std::cerr << "Error no: " << e.err() << std::endl;
+    }
+
+    cl_uint in_bytes = sizeof(input.pixels[0]) * input.width * input.height;
+    cl_uint out_bytes = sizeof(votes.accum[0]) * votes.width * votes.height * votes.nRadii;
+
+    cl_mem_flags in_flag = CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR;
+    cl_mem_flags out_flag = CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR;
+
+    cl::Buffer in_buf(context, in_flag, in_bytes, input.pixels);
+    cl::Buffer out_buf(context, out_flag, out_bytes, votes.accum);
+    cl::Buffer cnt_buf(context, out_flag, sizeof(total_votes), &total_votes);
+
+    // Setting kernel arguments
+    cl::Kernel kernel(*program, "CHT_vote");
+    kernel.setArg(0, votes.width);
+    kernel.setArg(1, votes.height);
+    kernel.setArg(2, votes.nRadii);
+    kernel.setArg(3, votes.r_min);
+    kernel.setArg(4, votes.r_step);
+    kernel.setArg(5, votes.pixel_threshold);
+    kernel.setArg(6, in_buf);
+    kernel.setArg(7, out_buf);
+    kernel.setArg(8, cnt_buf);
+
+    // Setting work group
+    cl_uint local_size = 32;
+    cl_uint global_size_x = (votes.width + local_size - 1) / local_size * local_size;
+    cl_uint global_size_y = (votes.height + local_size - 1) / local_size * local_size;
+    cl::NDRange local(local_size, local_size);
+    cl::NDRange global(global_size_x, global_size_y);
+
+    // Start running kernel
+    cmdQueue.enqueueNDRangeKernel(kernel, cl::NullRange, global, local);
+    cmdQueue.enqueueReadBuffer(out_buf, CL_TRUE, 0, out_bytes, votes.accum);
+    cmdQueue.enqueueReadBuffer(cnt_buf, CL_TRUE, 0, sizeof(total_votes), &total_votes);
 
     // After voting, filter significant votes based on the criterion specified in the instruction of the exam
     // each significant vote becomes an identified circle
+    const double vote_threshold = votes.threshold * total_votes / (votes.width * votes.height * votes.nRadii);
+    for (png_uint_32 y = 0; y < input.height; y++) {
+        for (png_uint_32 x = 0; x < input.width; x++) {
+            for (uint32_t r = 0; r < votes.nRadii; r++) {
+                uint32_t pos = y * input.width + x + r * input.width * input.height;
+
+                if (votes.accum[pos] > vote_threshold) {
+                    struct circle circle;
+
+                    circle.x     = x;
+                    circle.y     = y;
+                    circle.r     = votes.r_min + r * votes.r_step;
+                    circle.votes = votes.accum[pos];
+
+                    votes.circles.push_back(circle);
+                }
+            }
+        }
+    }
     
     // return the number of identified circles at this stage.
-    return 0; 
+    return votes.circles.size(); 
 }
 
 uint32_t extractCircles(accumulator& votes, circles& circles) {
-    cout << "\n[OCL]Implement extractCircles function in " << __FILE__ << " @ " << __LINE__; 
+    const int diff_dist = 20;
+    bool *is_merge = new bool [votes.circles.size()]();
+
+    // Find the similar circle and only pick one of them
+    for (uint32_t i = 0; i < votes.circles.size(); i++) {
+        if (is_merge[i]) continue;
+
+        is_merge[i] = true;
+        auto c1 = votes.circles[i];
+
+        std::vector<circle> similar;
+        similar.push_back(c1);
+
+        for (uint32_t j = 0; j < votes.circles.size(); j++) {
+            if (is_merge[j]) continue;
+
+            auto c2 = votes.circles[j];
+
+            if (abs((int32_t) c1.x - (int32_t) c2.x) < diff_dist && 
+                abs((int32_t) c1.y - (int32_t) c2.y) < diff_dist &&
+                abs((int32_t) c1.r - (int32_t) c2.r) < diff_dist) {
+                is_merge[j] = true;
+                similar.push_back(c2);
+            }
+        }
+
+        // Average all circles parameters
+        struct circle sum = { 0 };
+        for (const auto& c : similar) {
+            sum.x     += c.x;
+            sum.y     += c.y;
+            sum.r     += c.r;
+            sum.votes += c.votes;
+        }
+
+        int size = similar.size();
+        sum.x     /= size;
+        sum.y     /= size;
+        sum.r     /= size;
+        sum.votes /= size;
+
+        circles.data.push_back({sum.x, sum.y, sum.r, sum.votes});
+    }
     
     // the following line of code demonstrate how to put detected circles into the circles object
     // you shoudl remove it after you have extract circles from votes...
-    circles.data.push_back({100, 150, 200, 1000}); // store a circle @ (100, 150) with radius 200 and 1000 votes.
+    // circles.data.push_back({100, 150, 200, 1000}); // store a circle @ (100, 150) with radius 200 and 1000 votes.
 
-    return 0; 
+    return circles.data.size(); 
 }
 
 void freeAccumulator(accumulator& votes) {
-    cout << "\n[OCL]Implement freeAccumulator function in " << __FILE__ << " @ " << __LINE__;
+    delete [] votes.accum;
 }
